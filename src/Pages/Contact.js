37ec5ba1b6e08bdd9bb720682b4eb5de42ce4084
paridaabinash <abinashparida833@gmail.com>
2022-05-28 22:27:19 +0530

    first commit

---
+++ src/Pages/Contact.js
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+import '../App.css';
+
+export function Contact(){
+    return(
+        <div className="Contact">
+          <Header/>
+            <h1>Contact us</h1>
+        </div>
+    )
+}
